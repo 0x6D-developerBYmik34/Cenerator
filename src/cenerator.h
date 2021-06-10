cc19bf4485bf6ce850727bcf4c50a5b7b6e1d067
mikhayil <63368593+0x6D-developerBYmik34@users.noreply.github.com>
2021-06-10 11:55:11 +0300

    some edit code, add struct for params, AWAIT good

--- src/cenerator.h
+++ src/cenerator.h
@@ -3,6 +3,7 @@
 
 #include <stdio.h>
 #include <setjmp.h>
+#include <string.h>
 
 
 #define bool _Bool
@@ -11,44 +12,49 @@
 #define BEGIN_CORO() ({ \
     static coro_t awaiting_coro = {0}; \
     coro->awaiting_coro = &awaiting_coro; \
-    if(coro_is_suspend(coro)) longjmp(&(coro->coro_env), 1); \
+    printf("%i", sizeof(coro->awaiting_coro)); \
+    if(coro_is_suspend(coro)) longjmp((coro->coro_env), 1); \
     coro->status = RUNNING; \
 })
 
 
 #define YIELD(value) ({\
-    if(setjmp (&(coro->coro_env)) != 0) {\
-      memset(coro->coro_env, 0, sizeof(jmp_buf));\
+    if(setjmp((coro->coro_env)) != 0) {\
+      memset(coro->coro_env, 0, sizeof(coro->coro_env));\
       coro->status = RUNNING;\
       printf(" continue ");\
     } else {\
      coro->status = YIELDING;\
      coro->yield_value = value;\
      printf(" jump ");\
-     longjmp(&(coro->yields_env), 1);\
+     longjmp((coro->yields_env), 1);\
     }\
 })
 
 
 #define AWAIT(func) ({ \
-  if(setjmp (&(coro->coro_env)) != 0) { \
+  if(setjmp ((coro->coro_env)) != 0) { \
       if(coro_is_done(coro->awaiting_coro)) { \
-        memset(coro->coro_env, 0, sizeof(jmp_buf)); \
+        memset(coro->coro_env, 0, sizeof(coro->coro_env)); \
         memset(coro->awaiting_coro, 0, sizeof(coro_t)); \
+        printf("%i", sizeof(coro->awaiting_coro)); \
         printf(" continue "); \
         coro->status = RUNNING; \
       } else { \
         coro->status = YIELDING; \
+        printf("%i", sizeof(coro->awaiting_coro)); \
         coro->yield_value = coro_resume(coro->awaiting_coro); \
         printf(" jump is AWAIT "); \
-        longjmp(&(coro->yields_env), 1); \
+        longjmp((coro->yields_env), 1); \
       } \
     } else { \
       coro_inintial(coro->awaiting_coro, func); \
       coro->status = YIELDING; \
+      printf("%i", sizeof(coro->awaiting_coro)); \
       coro->yield_value = coro_resume(coro->awaiting_coro); \
+      printf("%i", sizeof(coro->awaiting_coro)); \
       printf (" jump is AWAIT "); \
-      longjmp(&(coro->yields_env), 1); \
+      longjmp((coro->yields_env), 1); \
     } \
     coro->yield_value; \
 })
@@ -56,6 +62,7 @@
 
 typedef struct coro_t_ coro_t;
 typedef int (*coro_function_t) (coro_t *coro);
+typedef struct str_params_coro coro_params;
 
 
 typedef enum enum_coro_status {
@@ -68,6 +75,9 @@ typedef enum enum_coro_status {
 } coro_status;
 
 
+struct str_params_coro { };
+
+
 struct coro_t_
 {
   coro_function_t function;
@@ -76,7 +86,7 @@ struct coro_t_
   jmp_buf coro_env;
   int yield_value;
   coro_t *awaiting_coro;
-  void *params;
+  coro_params *params;
 };
 
 
@@ -101,9 +111,9 @@ int coro_resume(coro_t *coro)
 {
   if (coro_is_done(coro)) return -1;
   
-  if (setjmp(&(coro->yields_env)) != 0) {
+  if (setjmp((coro->yields_env)) != 0) {
       printf(" na te ");
-      memset(coro->yields_env, 0, sizeof(jmp_buf));
+      memset(coro->yields_env, 0, sizeof(coro->yields_env));
       coro->status = SUSPEND;
       return coro->yield_value;
     } else {
@@ -111,8 +121,8 @@ int coro_resume(coro_t *coro)
       int return_value;
       return_value = (coro->function)(coro);
       coro->status = COMPLETE;
-      memset(coro->coro_env, 0, sizeof(jmp_buf));
-      memset(coro->yields_env, 0, sizeof(jmp_buf));
+      memset(coro->coro_env, 0, sizeof(coro->coro_env));
+      memset(coro->yields_env, 0, sizeof(coro->yields_env));
       printf(" end ");
       return return_value;
     }
